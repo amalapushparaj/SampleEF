1a32f89833208e261173f86de701d01788b91ed1
amala.puspharaj <amala.puspharaj@ASPIRE1683>
2017-08-24 12:04:50 +0530

    Add project files.

---
+++ SampleEF Sample Query.sql
@@ -0,0 +1,38 @@
+﻿CREATE TABLE [dbo].[Employees]
+(
+	[EmployeeId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[EmployeeName] VARCHAR(100) NOT NULL,
+	[DateofBirth] DATE NOT NULL,
+	[Gender] VARCHAR(50),
+	[DateofJoining] DATE NOT NULL
+)
+
+-- DROP TABLE [dbo].[Employees]
+
+CREATE TABLE [dbo].[EmployeeAddress]
+(
+	[EmployeeId] INT NOT NULL,
+	[Address1] VARCHAR(100) NOT NULL,
+	[Address] VARCHAR(100),
+	[City] VARCHAR(100) NOT NULL,
+	[State] VARCHAR(150) NOT NULL
+)
+
+-- DROP TABLE [dbo].[EmployeeAddress]
+
+CREATE TABLE [dbo].[Departments]
+(
+	[DepartmentId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[DepartmentName] VARCHAR(100) NOT NULL,
+	[Location] VARCHAR(100) 
+)
+
+-- DROP TABLE [dbo].[Departments]
+
+CREATE TABLE [dbo].[EmpDeptDetails]
+(
+	[DepartmentId] INT NOT NULL,
+	[EmployeeId] INT NOT NULL
+)
+
+-- DROP TABLE [dbo].[EmpDeptDetails]
---
+++ SampleEF/Module/GetEmployeesByDepartmentId_Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEF.Module
+{
+    public class GetEmployeesByDepartmentId_Result
+    {
+        public int EmployeeId { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; }
+    }
+}
